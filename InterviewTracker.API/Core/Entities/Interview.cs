0c8d46c6580cfb2819aa851e25d2942d91924f78
Ravan Hashimov <ravanhashimov@gmail.com>
2023-06-13 15:08:45 +0300

    Add Interview Entity

---
+++ InterviewTracker.API/Core/Entities/Interview.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterviewTracker.API.Core.Entities;
+
+public class Interview
+{
+    [Key] public long Id { get; set; }
+    public DateTime InterviewDate { get; set; }
+    public string IntervieweeName { get; set; }
+    public string InterviewerName { get; set; }
+    public int TotalQuestions { get; set; }
+    public int CorrectAnswers { get; set; }
+    public int IncorrectAnswers { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public string ConfidentialComment { get; set; } = "Normal";
+}
