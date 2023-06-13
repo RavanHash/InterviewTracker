3e2e825a2bd7a7d774c3403034db5a30181def8e
Ravan Hashimov <ravanhashimov@gmail.com>
2023-06-13 15:27:19 +0300

    Add InterviewsController

---
+++ InterviewTracker.API/Core/Context/InterviewsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewTracker.API.Core.Context;
+
+[Route("InterviewTrackerAPI/[controller]")]
+[ApiController]
+public class InterviewsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public InterviewsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+}
